278f9fe6779bc5916bfdde72bf3e87fa342621ea
WhizSid <whizsid@aol.com>
2020-03-15 15:41:41 +0530

    Updates

---
+++ precache-manifest.574ae3c207da6c9290b9ed358c48d7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6dc4df9c22f004a7c7ea1d6d3b3927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d0523563061187b120c",
+    "url": "/static/js/2.cf855411.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.cf855411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28109d6faa44a5094903",
+    "url": "/static/js/main.e18f09d3.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
